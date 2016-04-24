4f2ee8f12f3c409b956fad14ea675adb61616beb
jakkefaxx <jakkefaxx@gmail.com>
2016-04-24 11:53:08 +0200

    Changed this method blabla

--- app/src/main/java/com/outsourcify/outsourcify/TemplateActivity.java
+++ app/src/main/java/com/outsourcify/outsourcify/TemplateActivity.java
@@ -12,6 +12,7 @@ import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.ImageButton;
+//Added functionality
 
 public class TemplateActivity extends AppCompatActivity {
 
